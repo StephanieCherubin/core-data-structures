cc99863579dd2e7073317c51f2413a1a59154436
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-06-18 15:54:12 -0700

    only 1 test failing

--- Code/strings.py
+++ Code/strings.py
@@ -30,6 +30,24 @@ def find_index(text, pattern):
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_index here (iteratively and/or recursively)
 
+    starting_index = None
+    text_index = 0 
+    pattern_index = 0 
+
+    # base cases
+    if pattern == '' :
+        return 0
+    
+    for text_index, char in enumerate(text):
+        while pattern[pattern_index]== text[text_index]:
+            if starting_index is None:
+                starting_index = text_index
+            if len(pattern) - 1 == pattern_index:
+                return starting_index
+            pattern_index += 1
+            text_index += 1
+        pattern_index = 0
+    return None
 
 def find_all_indexes(text, pattern):
     """Return a list of starting indexes of all occurrences of pattern in text,
@@ -37,15 +55,57 @@ def find_all_indexes(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_all_indexes here (iteratively and/or recursively)
+    all_indexes = []
 
+    if len(pattern) == 0:
+        return list(range(len(text)))
+    if len(pattern) > len(text):
+        return all_indexes
+
+    text_index = 0
+    pattern_index = 0
+
+    while text_index < len(text):
+
+        if text[text_index] == pattern[pattern_index]:
+
+            # equal until last character in pattern
+            if pattern_index == len(pattern) - 1:
+
+                # calculate the start index and append to the result array
+                start_index = text_index - pattern_index
+                all_indexes.append(start_index)
+
+                # point the text index to be one ahead the start index
+                text_index = start_index + 1
+                pattern_index = 0
+
+            # equal in the pattern
+            else:
+
+                pattern_index += 1
+                text_index += 1
+
+        else:
+
+            # set back to the next index since it didnt pass all the pattern
+            if pattern_index != 0:
+                text_index = (text_index - pattern_index) + 1
+            else:
+                # set to next index
+                text_index += 1
+
+            pattern_index = 0
+
+    return all_indexes
 
 def test_string_algorithms(text, pattern):
     found = contains(text, pattern)
     print('contains({!r}, {!r}) => {}'.format(text, pattern, found))
-    # TODO: Uncomment these lines after you implement find_index
+
     index = find_index(text, pattern)
     print('find_index({!r}, {!r}) => {}'.format(text, pattern, index))
-    # TODO: Uncomment these lines after you implement find_all_indexes
+
     indexes = find_all_indexes(text, pattern)
     print('find_all_indexes({!r}, {!r}) => {}'.format(text, pattern, indexes))
 
