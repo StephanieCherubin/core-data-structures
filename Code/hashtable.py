0a59699ea0a94d75b0ec384afea6320be267572d
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-06-24 20:24:44 -0700

    I WAS MISSING A PARENTHESESga hashtable.pyga hashtable.py WTF? FML

--- Code/hashtable.py
+++ Code/hashtable.py
@@ -27,11 +27,15 @@ class HashTable(object):
     def load_factor(self):
         """Return the load factor, the ratio of number of entries to buckets.
         Best and worst case running time: ??? under what conditions? [TODO]"""
-        return self.size / len(self.buckets)
+        # return float(self.size)/float(len(self.buckets)) if self.size else 0
+
+        return self.size/len(self.buckets)
+
+
 
     def keys(self):
         """Return a list of all keys in this hash table.
-        Best and worst case running time: ??? under what conditions? [TODO]"""
+        Best and worst case running time: O(n^2) under what conditions? its a double for loop"""
         # Collect all keys in each of the buckets
         all_keys = []
 
@@ -78,6 +82,7 @@ class HashTable(object):
         # Find the bucket the given key belongs in
         index = self._bucket_index(key)
         bucket = self.buckets[index]
+
         # Check if an entry with the given key exists in that bucket
         entry = bucket.find(lambda key_value: key_value[0] == key)
         return entry is not None  # True or False
@@ -91,7 +96,7 @@ class HashTable(object):
         bucket = self.buckets[index]
         # Find the entry with the given key in that bucket, if one exists
         entry = bucket.find(lambda key_value: key_value[0] == key)
-        if entry is not None:  # Found
+        if entry:  # Found
             # Return the given key's associated value
             assert isinstance(entry, tuple)
             assert len(entry) == 2
@@ -115,15 +120,14 @@ class HashTable(object):
             # In this case, the given key's value is being updated
             # Remove the old key-value entry from the bucket first
             bucket.delete(entry)
-            self.size -=1
+            self.size -= 1
         # Insert the new key-value entry into the bucket in either case
         bucket.append((key, value))
         self.size += 1
         # TODO: Check if the load factor exceeds a threshold such as 0.75
-        # ...
-        # TODO: If so, automatically resize to reduce the load factor
         if self.load_factor() > 0.75:
-            self._resize
+        # TODO: If so, automatically resize to reduce the load factor
+            self._resize()
 
     def delete(self, key):
         """Delete the given key and its associated value, or raise KeyError.
@@ -134,7 +138,7 @@ class HashTable(object):
         bucket = self.buckets[index]
         # Find the entry with the given key in that bucket, if one exists
         entry = bucket.find(lambda key_value: key_value[0] == key)
-        if entry is not None:  # Found
+        if entry:  # Found
             # Remove the key-value entry from the bucket
             bucket.delete(entry)
             self.size -= 1
@@ -155,15 +159,16 @@ class HashTable(object):
         elif new_size is 0:
             new_size = len(self.buckets) / 2  # Half size
         # TODO: Get a list to temporarily hold all current key-value entries
-        temporary_entries = self.items() # O(b) with b being the number of buckets => O(n) 
+        temporary_entries_list = self.items() # O(b) with b being the number of buckets => O(n) 
         # TODO: Create a new list of new_size total empty linked list buckets
         self.__init__(new_size) #O(2b) => O(b)
         # TODO: Insert each key-value entry into the new list of buckets,
         # which will rehash them into a new bucket index based on the new size
         
-        for entry in temporary_entries: #O(n)
+        for entry in temporary_entries_list: #O(n)
             key = entry[0]
             value = entry[1]
+            # self.buckets[self._bucket_index(key)].append((key, value))
             self.set(key, value) #O(l) where l is the average length of the linked list
 
 
