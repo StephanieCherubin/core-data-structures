00828d2340a419a8f1dbaf26063c79923d5bf610
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-06-27 14:45:13 -0700

    2 tests pass

--- Code/binarytree.py
+++ Code/binarytree.py
@@ -16,24 +16,58 @@ class BinaryTreeNode(object):
     def is_leaf(self):
         """Return True if this node is a leaf (has no children)."""
         # TODO: Check if both left child and right child have no value
-        return ... and ...
+        if self.left is not None and self.right is not None:
+            return True
+        return False
 
     def is_branch(self):
         """Return True if this node is a branch (has at least one child)."""
         # TODO: Check if either left child or right child has a value
-        return ... or ...
+        if self.left is None and self.right is None:
+            return True
+        return False
 
     def height(self):
         """Return the height of this node (the number of edges on the longest
         downward path from this node to a descendant leaf node).
         TODO: Best and worst case running time: ??? under what conditions?"""
         # TODO: Check if left child has a value and if so calculate its height
-        ...
         # TODO: Check if right child has a value and if so calculate its height
         ...
-        # Return one more than the greater of the left height and right height
-        ...
+        # if self.root is None: 
+        #     return 0 ;  
+  
+        # else : 
 
+        #     # Compute the depth of each subtree 
+        #     left_height = height(self.left) 
+        #     right_height = height(self.right) 
+    
+        #     # Use the larger one 
+        #     if left_height > right_height: 
+        #         # Return one more than the greater of the left height and right height
+
+        #         return left_height + 1
+        #     else: 
+        #         # Return one more than the greater of the left height and right height
+
+        #         return right_height + 1
+        if self.root is None: 
+                return 0 
+            
+        # Base Case : height = 1 
+        if self.root.left is None and self.root.right is None: 
+            return 1
+        
+        # If left subtree is Null, recur for right subtree 
+        if self.root.left is None: 
+            return height(self.root.right)+1
+        
+        # If right subtree is Null , recur for left subtree 
+        if self.root.right is None: 
+            return height(self.root.left) +1 
+        
+        return max(height(self.root.left), height(self.root.right))+1
 
 class BinarySearchTree(object):
 
@@ -58,7 +92,7 @@ class BinarySearchTree(object):
         downward path from this tree's root node to a descendant leaf node).
         TODO: Best and worst case running time: ??? under what conditions?"""
         # TODO: Check if root node has a value and if so calculate its height
-        ...
+        return self.root.height()
 
     def contains(self, item):
         """Return True if this binary search tree contains the given item.
@@ -86,9 +120,9 @@ class BinarySearchTree(object):
         # Handle the case where the tree is empty
         if self.is_empty():
             # TODO: Create a new root node
-            self.root = ...
+            # self.root = ...
             # TODO: Increase the tree size
-            self.size ...
+            # self.size ...
             return
         # Find the parent node of where the given item should be inserted
         parent = self._find_parent_node_recursive(item, self.root)
@@ -101,7 +135,7 @@ class BinarySearchTree(object):
             # TODO: Create a new node and set the parent's right child
             parent.right = ...
         # TODO: Increase the tree size
-        self.size ...
+        # self.size ...
 
     def _find_node_iterative(self, item):
         """Return the node containing the given item in this binary search tree,
