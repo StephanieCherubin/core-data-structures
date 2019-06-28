d162c928897358020a86a869f92c3655a3501f50
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-06-28 16:58:07 -0700

    peek method passing

--- Code/stack.py
+++ Code/stack.py
@@ -3,8 +3,6 @@
 from linkedlist import LinkedList
 
 
-# Implement LinkedStack below, then change the assignment at the bottom
-# to use this Stack implementation to verify it passes all tests
 class LinkedStack(object):
 
     def __init__(self, iterable=None):
@@ -69,30 +67,35 @@ class ArrayStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        # TODO: Check if empty
+        if len(self.list) == 0:
+            return True
+        return False
 
     def length(self):
         """Return the number of items in this stack."""
-        # TODO: Count number of items
+        return len(self.list)
 
     def push(self, item):
         """Insert the given item on the top of this stack.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+        return self.list.append(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
-        # TODO: Return top item, if any
+        if self.is_empty():
+            return None
+        return self.list[-1]
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return top item, if any
+        if self.is_empty():
+            raise ValueError()
+        else:
+            return self.list.pop(0)
 
 
-# Implement LinkedStack and ArrayStack above, then change the assignment below
-# to use each of your Stack implementations to verify they each pass all tests
-Stack = LinkedStack
-# Stack = ArrayStack
+# Stack = LinkedStack
+Stack = ArrayStack
