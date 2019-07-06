d2e0520cccd1cbd4201e6a993e5eccdc12da890b
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-07-06 12:10:09 -0700

    [add] permutation function

--- Code/wordjumble.py
+++ Code/wordjumble.py
@@ -1,6 +1,4 @@
-from itertools import permutations 
-import re
-import sys
+
 
 def get_file_lines(filename='/usr/share/dict/words'):
     """Return a list of strings on separate lines in the given text file with
@@ -12,8 +10,65 @@ def get_file_lines(filename='/usr/share/dict/words'):
 
     return lines
 
+# generate permutations of a given list 
+def permutation(lst): 
+  
+    # If lst is empty then there are no permutations 
+    if len(lst) == 0: 
+        return [] 
+  
+    # If there is only one element in lst then, only 
+    # one permuatation is possible 
+    if len(lst) == 1: 
+        return [lst] 
+  
+    l = [] # empty list that will store current permutation 
+  
+    # Iterate the input(lst) and calculate the permutation 
+    for i in range(len(lst)): 
+       m = lst[i] 
+  
+       # Extract lst[i] or m from the list.  remLst is 
+       # remaining list 
+       remLst = lst[:i] + lst[i+1:] 
+  
+       # Generating all permutations where m is first 
+       # element 
+       for p in permutation(remLst): 
+           l.append([m] + p) 
+    return l 
+  
+  
+# Driver program to test above function 
+word = list('TEFON') 
+# words1 = ['TEFON', 'SOKIK', 'NIUMEM', 'SICONU']
+for p in permutation(word): 
 
-def solve_word_jumble(words, circles, final):
+  
+    s = ""
+  
+    # joins elements of list1 by '-' 
+    # and stores in sting s 
+    s = s.join(p) 
+      
+    # join use to join a list of 
+    # strings to a separator s 
+    print(s)
+
+def word_search(words, dictionary):
+    '''
+        Goes through words [list] and checks if its in the dictionary (set) of word_set
+        O(n) -> len of words
+        Return word_found -> list of words found in dict.
+    '''
+    word_found = []
+    for word in words:
+        if word in dictionary:
+            word_found.append(word)
+
+    return word_found
+
+def solve_word_jumble(word_perms):
     """Solve a word jumble by unscrambling four jumbles, then a final jumble.
     Parameters:
     - words: list of strings, each is the scrambled letters for a single word
@@ -25,8 +80,17 @@ def solve_word_jumble(words, circles, final):
     - final: list of strings in the same format as circles parameter that shows
         how the final jumble's letters are arranged into a word or phrase."""
     # Get all English words in the built-in dictionary
-    all_words = get_file_lines()
+    # all_words = get_file_lines()
     # TODO: Solve this word jumble with data structures and algorithms
+    final_words = []
+    word_set = get_file_lines()
+    for word in word_perms:
+        permuatation = permutation(word)
+        word_found = word_search(permuatation, word_set)
+        final_words.append(word_found)
+
+    print('hello')
+    return final_words
 
 
 def main():
@@ -35,13 +99,13 @@ def main():
     words1 = ['TEFON', 'SOKIK', 'NIUMEM', 'SICONU']
     circles1 = ['__O_O', 'OO_O_', '____O_', '___OO_']
     final1 = ['OO', 'OOOOOO']
-    solve_word_jumble(words1, circles1, final1)
+    solve_word_jumble(words1)
 
     # Word Jumble 2. Cartoon prompt for final jumble: "What a dog house is."
     words2 = ['TARFD', 'JOBUM', 'TENJUK', 'LETHEM']
     circles2 = ['____O', '_OO__', '_O___O', 'O____O']
     final2 = ['OOOO', 'OOO']
-    solve_word_jumble(words2, circles2, final2)
+    solve_word_jumble(words2)
 
 
 if __name__ == '__main__':
